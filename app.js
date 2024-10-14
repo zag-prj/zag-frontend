459a17ce30b7f1d4d825787def4bef5c9f695f2f
TropicSatern36 <hendry.ihlenfeldt@gmail.com>
2024-10-14 08:55:41 +0200

    moved html to public remove unused views

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+//Define routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,6 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Direct to routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
