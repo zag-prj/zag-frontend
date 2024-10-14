4e2452291464b6888aab6bde19b7c55ab3b61c6e
TropicSatern36 <hendry.ihlenfeldt@gmail.com>
2024-10-14 12:28:23 +0200

    fix index.html back to index.html

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path'); // Import the path module
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   //res.render('index', { title: 'Express' });
   // Return index.html
-  res.sendFile('./index.html');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
