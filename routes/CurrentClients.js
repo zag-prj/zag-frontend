33ee0f0e69d4e6a28795c01dfc6e34960a242e27
Aidan <141139206+MistaWasabi@users.noreply.github.com>
2024-11-05 19:27:36 +0200

    Add files via upload

--- routes/CurrentClients.js
+++ routes/CurrentClients.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+ 
+// Define the API endpoint URL
+const apiEndpoint = '/api';
+ 
+// Make a GET request to the API endpoint
+axios.get(apiEndpoint)
+  .then(response => {
+    // Handle the response data
+    const data = response.data;
+    console.log(data);
+  })
+  .catch(error => {
+ 
 
+    // Handle any errors that occur during the request
+    console.error('Error fetching data:', error);
+  });
 router.get('/', function (req, res, next) {
   res.render('CurrentClients', {
     title: 'Current Clients', user: {
