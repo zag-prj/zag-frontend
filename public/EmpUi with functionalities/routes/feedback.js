a40d06e74455ff095fc9b4a815361ca7bbf278e2
TropicSatern36 <hendry.ihlenfeldt@gmail.com>
2024-11-05 14:54:41 +0200

    Delete: public/EmpUi with functionalities/

--- public/EmpUi with functionalities/routes/feedback.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('feedback', { title: 'Customer Feedback' });
-});
-
-module.exports = router;
