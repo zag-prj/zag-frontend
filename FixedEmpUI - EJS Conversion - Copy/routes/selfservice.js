64b666a48e69643329411be9cecbe9b9b4e5b114
TropicSatern36 <hendry.ihlenfeldt@gmail.com>
2024-11-05 14:06:39 +0200

    Fixed structior

--- FixedEmpUI - EJS Conversion - Copy/routes/selfservice.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('selfservice', { title: 'Self Service' });
-});
-
-module.exports = router;
